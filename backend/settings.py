a2984ab4c87cc96285132f6f070f4c724154423d
unknown <ferdinandimeka20@gmail.com>
2023-07-23 12:48:32 +0100

    cleanup

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-8+ca2==zhq_-o_vx&#s@f%68d(4xkrnb!#kn9-5)(k-yi-c3n6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
