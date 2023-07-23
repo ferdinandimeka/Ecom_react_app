f137309d16362ca9d357023a5d6f94a3bf687f16
unknown <ferdinandimeka20@gmail.com>
2023-07-23 21:53:11 +0100

    build

--- backend/urls.py
+++ backend/urls.py
@@ -28,8 +28,5 @@ urlpatterns = [
     path('',TemplateView.as_view(template_name='index.html')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
